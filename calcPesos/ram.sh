90a3f8668ac2f8768bdf2e5ce104c039d0fc78c6
sg1o <sergiobenllochlopez@gmail.com>
2021-05-13 13:59:55 +0200

    Ram usage added

---
+++ calcPesos/ram.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#Author: Sergio Benlloch, serbenl1
+#Script to get the average, maximum and minimum use of the ram of an executable
+#Data in 
+#Use:
+#    -v or --verbose for activate comments';
+#    -b or -binary to pass binary to execute';
+#Mprof is needed for the execution of this script
+
+calculo () {
+
+    count=0;
+    total=0;
+    max=0;
+    min=$(cat salidaInVar | head -2);
+
+    for elemento in $(cat salidaInVar)
+    do
+        total=$(echo $total+$elemento | bc )
+        ((count++))
+
+        if (( $(echo "$elemento > $max" |bc -l) )); then
+            max=$elemento;
+        fi
+
+        if (( $(echo "$min > $elemento" |bc -l) )); then
+            min=$elemento;
+        fi
+    done
+
+    if [ "$VERBOSE" = true ]; then
+        echo -n "Avg: ";
+        echo "scale=2; $total / $count" | bc;
+        echo 'Max: ' $max;
+        echo 'Min: ' $min;
+    else
+        echo "scale=2; $total / $count" | bc;
+        echo $max;
+        echo $min;
+    fi
+
+}
+
+VERBOSE=false
+
+for arg in "$@"
+do
+    case $arg in
+        -v|--verbose)
+        VERBOSE=true
+        shift
+        ;;
+        -b|--binary)
+        BINARY=$2
+        shift
+        shift
+        ;;
+    esac
+done
+
+if [[ -x "$BINARY" ]]
+then
+    if [ "$VERBOSE" = true ]; then
+    echo '[*]Running mprof with' $BINARY
+    fi
+else
+    echo "File '$BINARY' is not executable or found"
+    echo '[!]   Use:';
+    echo '      -v or --verbose for activate comments';
+    echo '      -b or -binary to pass binary to execute';
+    exit;
+fi
+
+if [ -f /tmp/salidaMprof ]; then
+    rm /tmp/salidaMprof;
+fi
+
+mprof run --output /tmp/salidaMprof $BINARY &>/dev/null
+
+if [ -f /tmp/salidaMprof ]; then
+
+    if [ "$VERBOSE" = true ]; then
+        echo '[*]Successfully executed';
+    fi
+    
+    cat /tmp/salidaMprof | awk -v var=val '{print $3}' > salidaInVar; 
+    calculo;
+    
+else
+
+    if [ "$VERBOSE" = true ]; then
+        echo '[!]Ejecutado con problemas, Funcionamiento:';
+        echo '      -v or --verbose for activate comments';
+        echo '      -b or -binary to pass binary to execute';
+    fi
+    
+fi
