ddd21cf5987561efbbe6030a0a33460501575fbb
sg1o <sergiobenllochlopez@gmail.com>
2021-07-01 18:09:42 +0200

    Genetic Algorithm: cambios final

--- optmizacionAG/code/fin.py
+++ optmizacionAG/code/fin.py
@@ -1,21 +1,48 @@
 import time
+import os
+import subprocess
+
+def executionWithOutput(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+    return (result.stdout, result.stderr)
+
+def error(pathFile, salida, error, lineaComp):
+    file = open(pathFile + 'errorCompilacion.txt', 'a')
+    file.write('Error en compilacion.\nError GCC: \n' +
+                str(error) + '\nSalida GCC:\n' + str(salida) + '\n')
+    file.write('Linea de compilacion:\n' + str(lineaComp))
+    file.close()
 
 def compilarIndividuos(directorio,listaACompilar, programa, dependencias, flagsDependencias):
     directorioComparacion = directorio + '/comparacionFinal/'
-    os.system('mkdir ' + directorioComparacion)
+    os.mkdir(directorioComparacion)
     cont = 0
     for opt in listaACompilar:
         directorioOpt = directorioComparacion + 'Optimizacion' + str(cont) + '/'
-        os.system('mkdir ' + directorioOpt
+        os.mkdir(directorioOpt)
         nombreCompilacion = directorioOpt + 'optimizacion' + str(cont)
         lineaComp = 'gcc ' + programa + ' ' + dependencias + ' -o ' + nombreCompilacion + ' ' + flagsDependencias + opt
         (out, err) = executionWithOutput(lineaComp)
+        if err!='' or out!='':
+            error(directorioOpt, out, err, lineaComp)
         cont += 1
     return directorioComparacion
 
-def comparacion(mejorCromosoma, programa, dependencias, flagsDependencias):
-
-    print('Se compara y ' + str(guardar))
+def comparacion(directorioBase, mejorCromosoma, programa, dependencias, flagsDependencias):
+    estandar = ''
+    opt0 = '-O0'
+    opt1 = '-O1'
+    opt2 = '-O2'
+    opt3 = '-O3'
+    size = '-Os'
+    fast = '-Ofast'
+    mejor = ''
+    for tupla in mejorCromosoma.tuplas:
+        if tupla[1]:
+            mejor += ' ' + tupla[0]
+    listaOpt = [estandar, opt0, opt1, opt2, opt3, size, fast, mejor]
+    directorio = compilarIndividuos(directorioBase, listaOpt, programa, dependencias, flagsDependencias)
 
 def salidaFin(historico, directorioBase, Gen, limite, tiempo_inicio, programa, dependencias, flagsDependencias):
     print('\n\n\033[1;36m┌────────────────────────────────────────────────────────┐')
@@ -29,7 +56,7 @@ def salidaFin(historico, directorioBase, Gen, limite, tiempo_inicio, programa, d
         print(' Ejecución finalizada por convergencia ejecución.\n')
     else:
         print('Límite fuera de rango')
-    carpeta = direcotorioBase.splie('/')[-1]
+    carpeta = directorioBase.split('/')[-1]
     print(' Se han generado automaticamente diferentes archivos de\n' +
         ' estadísticas de la ejecución, los puedes encontrar en la\n' +
         ' carpeta  ' + carpeta +' en el path que has\n' +
@@ -61,4 +88,4 @@ def salidaFin(historico, directorioBase, Gen, limite, tiempo_inicio, programa, d
             cont+=1
     print('\n')
     if comparar:
-        comparacion(ultimoSeleccionado, programa, dependencias, flagsDependencias)
+        comparacion(directorioBase, ultimoSeleccionado, programa, dependencias, flagsDependencias)
