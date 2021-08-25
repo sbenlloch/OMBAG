754aeebf18aad81575ed7683591fdfa5d3f82393
sg1o <sergiobenllochlopez@gmail.com>
2021-08-25 19:25:28 +0200

    Versión Tradicional

--- versionTradicional/test/peso.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#This script pretends to return user size of program in bytes
-#Only with one number for do more practice utilization of other algorithms
-#
-#Author: Sergio Benlloch, sg1o
-#Version: 0.1
-
-du -b $1 | awk '{print $1}'
